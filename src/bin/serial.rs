347f039d0829b129a072a65d185f3a2bbd386f41
SebastianJL <johannes.lade@yahoo.com>
2022-11-18 15:20:52 +0100

    Make code work for N dimensional particles. (i.e. different values of DIM).

--- src/bin/serial.rs
+++ src/bin/serial.rs
@@ -1,12 +1,19 @@
 use mass_assignment::{DIM, MAX, MIN, N_GRID, N_PARTICLES, coordinates::{SpaceCoordinate, GridCoordinate, FromSpaceCoordinate}};
 
-use ndarray::{Array2, Array3};
+use mass_assignment::{
+    coordinates::{FromSpaceCoordinate, GridCoordinate, SpaceCoordinate},
+    DIM, MAX, MIN, N_GRID, N_PARTICLES,
+};
+
+use ndarray::{Array, Array2, Dim};
 use rand::Rng;
+
+type MassGrid = Array<i32, Dim<[usize; DIM]>>;
+type ParticleArray = Array2<SpaceCoordinate>;
 
 fn main() {
     let particles = generate_particles();
-    let mut mass_grid = Array3::<i32>::zeros([N_GRID as usize, N_GRID as usize, N_GRID as usize]);
-
+    let mut mass_grid = MassGrid::zeros([N_GRID; DIM]);
     assign_masses(&particles, &mut mass_grid);
 
     // dbg!(&mass_grid);
@@ -16,8 +23,8 @@ fn main() {
 
 /// Generate random particles. Particles are layed out as a simple array with shape (N_PARTICLE, DIM)
 /// that describe coordinates of the particle.
-fn generate_particles() -> Array2<SpaceCoordinate> {
-    let mut particles = Array2::<SpaceCoordinate>::zeros([N_PARTICLES, DIM]);
+fn generate_particles() -> ParticleArray {
+    let mut particles = ParticleArray::zeros([N_PARTICLES, DIM]);
     let mut rng = rand::thread_rng();
     for particle_coord in particles.iter_mut() {
         *particle_coord = rng.gen_range(MIN..MAX).into();
@@ -26,13 +33,15 @@ fn generate_particles() -> Array2<SpaceCoordinate> {
 }
 
 /// Assign masses according to nearest grid point algorithm.
-fn assign_masses(particles: &Array2<SpaceCoordinate>, mass_grid: &mut Array3<i32>) {
+fn assign_masses(particles: &ParticleArray, mass_grid: &mut MassGrid) {
     for space_coords in particles.outer_iter() {
-        let grid_coords = [
-            GridCoordinate::from_space_coordinate(space_coords[0]),
-            GridCoordinate::from_space_coordinate(space_coords[1]),
-            GridCoordinate::from_space_coordinate(space_coords[2]),
-            ];
+        // Todo: Maybe replace with macro for ndim support.
+        let grid_coords: [GridCoordinate; DIM] = space_coords
+            .into_iter()
+            .map(|coord| GridCoordinate::from_space_coordinate(*coord))
+            .collect::<Vec<GridCoordinate>>()
+            .try_into()
+            .unwrap();
         mass_grid[grid_coords] += 1;
     }
 }
