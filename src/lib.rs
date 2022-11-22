4452f379d168f6b1512cafe21e57ccc93eb4177a
SebastianJL <johannes.lade@yahoo.com>
2022-11-22 16:45:25 +0100

    Change (MIN, MAX) from (-1, 1) to (-0.5, 0.5).

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,8 @@ pub mod coordinates;
 pub type Float = f32;
 
 /// Min value of space in simulation.
-pub const MIN: Float = -1.;
+pub const MIN: Float = -0.5;
 /// Max value of space in simulation.
-pub const MAX: Float = 1.;
+pub const MAX: Float = 0.5;
 /// Number of dimensions of space.
 pub const DIM: usize = 2;
