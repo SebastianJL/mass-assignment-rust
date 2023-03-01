ea23c51e5015b7cf5304d9fd1ff7b661f4bab700
SebastianJL <johannes.lade@yahoo.com>
2023-03-01 15:38:58 +0000

    Add plot_strong_scaling.py.
    Only data parsing implemented.

---
+++ plot_strong_scaling.py
@@ -0,0 +1,69 @@
+import argparse
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def init_argparse() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        # usage="%(prog)s [OPTION] [FILE]...",
+        description="Plot speedup of run files."
+    )
+    parser.add_argument(
+        "-n", "--no-show", action="store_false",
+        help="Don't show the figures.",
+        dest="show", default=True,
+    )
+    parser.add_argument(
+        "-s", "--save", action="store_true",
+        help="Save figures", default=False,
+    )
+    parser.add_argument('files', nargs='+')
+    return parser
+
+
+def parse_data(file):
+    columns = ['seed', 'n_particles', 'n_grid', 'n_threads', 'total_mass', 'runtime']
+    with open(file) as in_file:
+        line_iter = iter(in_file)
+        data = []
+        while True:
+            try:
+                # Skip first two lines of each block
+                next(line_iter)
+                next(line_iter)
+            except StopIteration:
+                break
+            row = [line.split(f"{column} = ")[1].strip() for column, line in zip(columns, line_iter)]
+            data.append(row)
+    df = pd.DataFrame(data, columns=columns)
+
+    # Parse dtypes.
+    for column in columns:
+        if column in ['runtime']:
+            df[column] = pd.to_timedelta(df[column])
+        elif column in ['seed', 'n_particles', 'n_grid', 'n_threads', 'total_mass']:
+            df[column] = pd.to_numeric(df[column])
+        else:
+            raise "Encountered unknown column"
+
+    return df
+
+
+def main() -> None:
+    parser = init_argparse()
+    args = parser.parse_args()
+    for file in args.files:
+        data = parse_data(file)
+        print(data)
+        print(data.dtypes)
+
+        fig = plt.subplots(1)
+        if args.save:
+            fig.save(f"{file}_plot.svg")
+
+    if args.show:
+        plt.show()
+
+
+if __name__ == "__main__":
+    main()
