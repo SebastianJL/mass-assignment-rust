e487388b8b30bbacabc4d7a250aa4a9064d91ea3
SebastianJL <johannes.lade@yahoo.com>
2023-03-23 13:18:44 +0000

    Plot mean of several runs.

--- parallel/plot_strong_scaling.py
+++ parallel/plot_strong_scaling.py
@@ -1,6 +1,7 @@
 import argparse
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
 
 def init_argparse() -> argparse.ArgumentParser:
@@ -47,7 +48,7 @@ def parse_data(file):
     # Parse dtypes.
     for column in columns:
         if column in ['runtime']:
-            df[column] = pd.to_timedelta(df[column])
+            df[column] = pd.to_timedelta(df[column]).values.astype(np.int64)
         elif column in ['seed', 'n_particles', 'n_grid', 'n_threads', 'total_mass']:
             df[column] = pd.to_numeric(df[column])
         else:
@@ -65,7 +66,26 @@ def main() -> None:
         # print(data.dtypes)
 
         fig, ax = plt.subplots(1)
-        ax.plot(data['n_threads'], data['runtime'][0] / data['runtime'], 'o', label="measured")
+        print(data.dtypes)
+        print(data.head())
+        # exit()
+        d = data[['n_threads', 'runtime']].groupby('n_threads')
+        means = d.mean(numeric_only=True).reset_index()
+        # mins = d.min(numeric_only=True).reset_index()
+        # # stds = d.std(numeric_only=True).reset_index()
+        # # stderr = stds['runtime'].values / np.sqrt(d.count()['runtime'].values)
+        # # stderr = stderr['runtime'].values
+        # # print(stds)
+        # # print(d.count())
+        # # print(stderr)
+        # # exit()
+        speedup = means['runtime'][0] / means['runtime']
+        # speedup_mins = means['runtime'][0] / mins['runtime']
+        # relerr = stderr / means['runtime'].values + stderr[0] / means['runtime'].values[0]
+        # speedup_err = relerr * speedup
+        ax.plot(data['n_threads'], data['runtime'][0]/data['runtime'], 'o', label="all")
+        ax.plot(means['n_threads'], speedup, 'o', label="mean")
+        # ax.plot(mins['n_threads'], speedup_mins,'o', label="fastest")
         ax.plot(data['n_threads'], data['n_threads'], label='ideal')
         ax.set_xlabel('# threads')
         ax.set_ylabel('speedup')
